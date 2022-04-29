f7383455052bfa905cfca9ed220a60d7796ec29e
midas <midas117@knou.ac.kr>
2022-04-29 22:53:52 +0900

    chore: 강의 테이블 정의(schema.sql), 임시 데이터 등록(data.sql)

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO lectures(name, lecturer, price) VALUES ('백엔드가 좋아', '백둥이', 10000);
+INSERT INTO lectures(name, lecturer, price) VALUES ('프론트가 좋아', '프롱이', 10000);
---
+++ server/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+-- 강의 데이터
+CREATE TABLE lectures
+(
+    lecutre_id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    lecturer VARCHAR(50) NOT NULL,
+    price DECIMAL NOT NULL,
+    thumbnail_image_url VARCHAR(255) DEFAULT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at DATETIME DEFAULT NULL,
+    PRIMARY KEY (lecutre_id)
+);
