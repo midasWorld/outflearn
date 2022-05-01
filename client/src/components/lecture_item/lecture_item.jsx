bf5d78b4550f40ba0d80c87fa6f2181ff765d85b
midas <midas117@knou.ac.kr>
2022-05-01 21:07:11 +0900

    [Front] feat: 강의 목록 조회 구현

--- client/src/components/lecture_item/lecture_item.jsx
+++ client/src/components/lecture_item/lecture_item.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "./lecture_item.module.css";
 
-const LectureItem = (props) => <h1>{props.lecture.name}</h1>;
+const LectureItem = ({ lecture }) => (
+  <li className={styles.container}>
+    <div className={styles.lecture}>
+      <img
+        className={styles.thumbnail}
+        src={lecture.thumbnail_image_url}
+        alt="lecture thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.name}>{lecture.name}</p>
+        <p className={styles.lecturer}>{lecture.lecturer}</p>
+        <p className={styles.price}>{lecture.price.toLocaleString()}</p>
+      </div>
+    </div>
+  </li>
+);
 
 export default LectureItem;
--- client/src/components/lecture_list/lecture_list.jsx
+++ client/src/components/lecture_list/lecture_list.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import LectureItem from "../lecture_item/lecture_item";
+import styles from "./lecture_list.module.css";
 
 const LectureList = (props) => (
-  <ul>
+  <ul className={styles.lectures}>
     {props.lectures.map((lecture) => (
       <LectureItem key={lecture.lectureId} lecture={lecture} />
     ))}
