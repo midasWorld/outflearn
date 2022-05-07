476cfa4b8249a6083706cff656b82dda76ee7866
midas <midas117@knou.ac.kr>
2022-05-07 11:55:34 +0900

    [Front] refactor: 주문상세내역 화면 타이틀 변경

--- client/src/components/order_details/order_details.jsx
+++ client/src/components/order_details/order_details.jsx
@@ -11,7 +11,7 @@ const OrderDetails = (props) => {
   return (
     <>
       <h2>
-        구매 상세 내역<span>(주문번호: {order.orderId})</span>
+        주문 상세 내역<span>(주문번호: {order.orderId})</span>
       </h2>
       <div className={styles.container}>
         <OrderLectureList lecture={order.lecture} voucher={order.voucher} />
