988a6e65f19fb7075b5a3f0f1d2b26e7d71e1bd5
midas <midas117@knou.ac.kr>
2022-05-07 10:17:40 +0900

    [Front] feat: 주문 상세내역 - 강의 정보 구현 완료

--- client/src/components/order_details/order_details.jsx
+++ client/src/components/order_details/order_details.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import OrderLectureList from "../order_lecture_list/order_lecture_list";
 import OrderPayment from "../order_payment/order_payment";
 import styles from "./order_details.module.css";
 
-const OrderDetails = ({ order }) => {
+const OrderDetails = (props) => {
+  const location = useLocation();
+  const order = location.state.order;
+
   return (
-    <div className={styles.container}>
-      <OrderLectureList />
-      <OrderPayment />
-    </div>
+    <>
+      <h2>
+        구매 상세 내역<span>(주문번호: {order.orderId})</span>
+      </h2>
+      <div className={styles.container}>
+        <OrderLectureList lecture={order.lecture} voucher={order.voucher} />
+        <OrderPayment />
+      </div>
+    </>
   );
 };
 
--- client/src/components/order_lecture_list/order_lecture_list.jsx
+++ client/src/components/order_lecture_list/order_lecture_list.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import styles from "./order_lecture_list.module.css";
 
-const OrderLectureList = (props) => {
+const OrderLectureList = ({ lecture, voucher }) => {
+  const discountAmount = (lecture.price * voucher.percent) / 100;
+  const amount = lecture.price - discountAmount;
+
   return (
     <section className={styles.container}>
-      <h1>Order Lecture List</h1>
+      <div className={styles.item}>
+        <div className={styles.lecture}>
+          <img
+            className={styles.thumbnail}
+            src={lecture.thumbnail_image_url}
+            alt="lecture thumbnail"
+          />
+          <div className={styles.metadata}>
+            <p className={styles.name}>{lecture.name}</p>
+          </div>
+          <div className={styles.price}>
+            {voucher.voucherId === null ? (
+              ""
+            ) : (
+              <p className={styles.before_amount}>
+                {lecture.price.toLocaleString()}
+              </p>
+            )}
+            <p className={styles.amount}>{amount.toLocaleString()}</p>
+          </div>
+        </div>
+        {voucher.voucherId === null ? (
+          ""
+        ) : (
+          <div className={styles.voucher}>
+            <span className={styles.voucher_arrow}>
+              <i class="fa-solid fa-arrow-up-long"></i>
+            </span>
+            적용된 할인 : <span>{voucher.name} 할인 쿠폰</span>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
